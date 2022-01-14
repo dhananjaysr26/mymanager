c6decd3871b7cd605e90a3663e6cba206282f3b6
dhananjaysr26 <dhananjaysr26@gmail.com>
2022-01-14 20:42:39 +0530

    first commit

---
+++ src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Profile.css'
+function Profile() {
+    alert("eee")
+    console.log("Hello")
+    return (
+        <div className='container'>
+            <h1>I am profile pageI am profile pageI am profile page</h1>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default Profile
